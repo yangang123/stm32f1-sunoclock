00abb0302bd1813342c5dfb4ee19a98ec1db86db
David DiPaola <DavidDiPaola@users.noreply.github.com>
2017-10-18 19:49:35 -0400

    main.c,stm32f1-common: fixed RCC register definitions, deleted io.h

--- main.c
+++ main.c
@@ -14,25 +14,25 @@ static void
 _setup_clock(void) {
 	/* see RM0008, figure 11 for clock tree diagram */
 
-	RCC_CLOCK |= RCC_CLOCK_HSE_ENABLE;
-	while ((RCC_CLOCK & RCC_CLOCK_HSE_READY) == 0) {}
+	*RCC_CLOCK |= RCC_CLOCK_HSE_ENABLE;
+	while ((*RCC_CLOCK & RCC_CLOCK_HSE_READY) == 0) {}
 
-	RCC_CONFIG = (RCC_CONFIG & ~(RCC_CONFIG_SYSCLK_MASK)) | RCC_CONFIG_SYSCLK_HSE;
-	while ((RCC_CONFIG & RCC_CONFIG_SYSCLK_STATUS_MASK) != RCC_CONFIG_SYSCLK_STATUS_HSE) {}
+	*RCC_CONFIG = (*RCC_CONFIG & ~(RCC_CONFIG_SYSCLK_MASK)) | RCC_CONFIG_SYSCLK_HSE;
+	while ((*RCC_CONFIG & RCC_CONFIG_SYSCLK_STATUS_MASK) != RCC_CONFIG_SYSCLK_STATUS_HSE) {}
 
-	RCC_CLOCK &= ~(RCC_CLOCK_HSI_ENABLE);
+	*RCC_CLOCK &= ~(RCC_CLOCK_HSI_ENABLE);
 
-	u32 config = RCC_CONFIG;
+	u32 config = *RCC_CONFIG;
 	config = (config & ~(RCC_CONFIG_APB2_MASK)) | RCC_CONFIG_APB2_DIV1;
 	config = (config & ~(RCC_CONFIG_APB1_MASK)) | RCC_CONFIG_APB1_DIV1;
 	config = (config & ~(RCC_CONFIG_AHB_MASK )) | RCC_CONFIG_AHB_DIV1;
-	RCC_CONFIG = config;
+	*RCC_CONFIG = config;
 }
 
 /* depends on _setup_clock() */
 static void
 _setup_led(void) {
-	RCC_APB2ENABLE |= RCC_APB2ENABLE_GPIOC;  /* see RM0008, figure 8 for clock tree diagram */
+	*RCC_APB2ENABLE |= RCC_APB2ENABLE_GPIOC;  /* see RM0008, figure 8 for clock tree diagram */
 
 	u32 config = *GPIOC_CONFIGHIGH;
 	config = (config & ~(GPIOC_CONFIGHIGH_13CONFIG_MASK)) | GPIOC_CONFIGHIGH_13CONFIG_GENERALPUSHPULL;
@@ -45,7 +45,7 @@ _setup_led(void) {
 /* depends on _setup_clock() */
 static void
 _setup_timer6(void) {
-	RCC_APB1ENABLE |= RCC_APB1ENABLE_TIMER6;  /* see RM0008, figure 11 for clock tree diagram */
+	*RCC_APB1ENABLE |= RCC_APB1ENABLE_TIMER6;  /* see RM0008, figure 11 for clock tree diagram */
 
 	*TIMER6_PRESCALER = 23999;
 
@@ -69,17 +69,18 @@ _setup_rtc(void) {
 			* enable the power and backup interface clocks by setting the PWREN and BKPEN bits in the RCC_APB1ENR register
 			* set the DBP bit the Power Control Register (PWR_CR) to enable access to the Backup registers and RTC
 	*/
-	RCC_APB1ENABLE |= (RCC_APB1ENABLE_POWER | RCC_APB1ENABLE_BACKUP);
+	*RCC_APB1ENABLE |= (RCC_APB1ENABLE_POWER | RCC_APB1ENABLE_BACKUP);
 	*POWER_CONTROL |= POWER_CONTROL_BACKUPWRITEENABLE;
 
-	/* TODO shouldn't this only be done once? */
-	RCC_BACKUPCONTROL |= RCC_BACKUPCONTROL_LSEENABLE;
-	while ((RCC_BACKUPCONTROL & RCC_BACKUPCONTROL_LSEREADY) == 0) {}
+	if ((*RCC_BACKUPCONTROL & RCC_BACKUPCONTROL_LSEREADY) == 0) {
+		*RCC_BACKUPCONTROL |= RCC_BACKUPCONTROL_LSEENABLE;
+		while ((*RCC_BACKUPCONTROL & RCC_BACKUPCONTROL_LSEREADY) == 0) {}
+	}
 
-	u32 control = RCC_BACKUPCONTROL;
+	u32 control = *RCC_BACKUPCONTROL;
 	control |= RCC_BACKUPCONTROL_RTCCLOCKENABLE;
 	control = (control & ~(RCC_BACKUPCONTROL_RTCCLOCKSELECT_MASK)) | RCC_BACKUPCONTROL_RTCCLOCKSELECT_LSE;
-	RCC_BACKUPCONTROL = control;
+	*RCC_BACKUPCONTROL = control;
 }
 
 void
