26dfe312d4fa1379862fca460380042ee46f506d
David DiPaola <DavidDiPaola@users.noreply.github.com>
2018-04-06 14:17:30 -0400

    commmon: rcc: updated to latest

--- main.c
+++ main.c
@@ -14,27 +14,27 @@ static const size LSE_FREQ =   32768;  /* aka LSE OSC */
 
 static void
 _setup_clock(void) {
-	/* see RM0008, figure 11 for clock tree diagram */
+	/* see RM0008 figure 8 for clock tree diagram */
 
-	*RCC_CLOCK |= RCC_CLOCK_HSE_ENABLE;
-	while ((*RCC_CLOCK & RCC_CLOCK_HSE_READY) == 0) {}
+	RCC->CR |= rcc_CR_HSEON;
+	while ((RCC->CR & rcc_CR_HSERDY) == 0) {}
 
-	*RCC_CONFIG = (*RCC_CONFIG & ~(RCC_CONFIG_SYSCLK_MASK)) | RCC_CONFIG_SYSCLK_HSE;
-	while ((*RCC_CONFIG & RCC_CONFIG_SYSCLK_STATUS_MASK) != RCC_CONFIG_SYSCLK_STATUS_HSE) {}
+	RCC->CFGR = (RCC->CFGR & ~(rcc_CFGR_SW_MASK)) | rcc_CFGR_SW_HSE;
+	while ((RCC->CFGR & rcc_CFGR_SWS_MASK) != rcc_CFGR_SWS_HSE) {}
 
-	*RCC_CLOCK &= ~(RCC_CLOCK_HSI_ENABLE);
+	RCC->CR &= ~(rcc_CR_HSION);
 
-	u32 config = *RCC_CONFIG;
-	config = (config & ~(RCC_CONFIG_APB2_MASK)) | RCC_CONFIG_APB2_DIV1;
-	config = (config & ~(RCC_CONFIG_APB1_MASK)) | RCC_CONFIG_APB1_DIV1;
-	config = (config & ~(RCC_CONFIG_AHB_MASK )) | RCC_CONFIG_AHB_DIV1;
-	*RCC_CONFIG = config;
+	u32 cfgr = RCC->CFGR;
+	cfgr = (cfgr & ~(rcc_CFGR_PPRE2_MASK)) | rcc_CFGR_PPRE2_DIV1;  /* APB2 */
+	cfgr = (cfgr & ~(rcc_CFGR_PPRE1_MASK)) | rcc_CFGR_PPRE1_DIV1;  /* APB1 */
+	cfgr = (cfgr & ~(rcc_CFGR_HPRE_MASK )) | rcc_CFGR_HPRE_DIV1;   /* AHB */
+	RCC->CR = cfgr;
 }
 
 /* depends on _setup_clock() */
 static void
 _setup_led(void) {
-	*RCC_APB2ENABLE |= RCC_APB2ENABLE_GPIOC;  /* see RM0008, figure 8 for clock tree diagram */
+	RCC->APB2ENR |= rcc_APB2ENR_IOPCEN;  /* see RM0008 figure 8 for clock tree diagram */
 
 	u32 crh = GPIOC->CRH;
 	crh = (crh & ~(gpio_CRH_CNF13_MASK )) | gpio_CRH_CNF13_OUT_GPPP;
@@ -59,7 +59,7 @@ _pwm_timer1_set(u8 red, u8 green, u8 blue) {
 /* depends on _setup_clock() */
 static void
 _setup_pwm_timer1(void) {
-	*RCC_APB2ENABLE |= (RCC_APB2ENABLE_TIMER1 | RCC_APB2ENABLE_GPIOA | RCC_APB2ENABLE_ALTERNATEFUNCTIONIO);  /* see RM0008, figure 8 for clock tree diagram */
+	RCC->APB2ENR |= (rcc_APB2ENR_TIM1EN | rcc_APB2ENR_IOPAEN | rcc_APB2ENR_AFIOEN);  /* see RM0008, figure 8 for clock tree diagram */
 
 	u32 crh = GPIOA->CRH;
 	/* timer1 channel2 pin init */
@@ -104,23 +104,23 @@ _setup_pwm_timer1(void) {
 /* depends on _setup_clock() */
 static void
 _setup_rtc(void) {
-	/* RM0008, 18.1:
+	/* RM0008 18.1:
 		To enable access to the Backup registers and the RTC, proceed as follows:
 			* enable the power and backup interface clocks by setting the PWREN and BKPEN bits in the RCC_APB1ENR register
 			* set the DBP bit the Power Control Register (PWR_CR) to enable access to the Backup registers and RTC
 	*/
-	*RCC_APB1ENABLE |= (RCC_APB1ENABLE_POWER | RCC_APB1ENABLE_BACKUP);
+	RCC->APB1ENR |= (rcc_APB1ENR_PWREN | rcc_APB1ENR_BKPEN);
 	PWR->CR |= pwr_CR_DBP;
 
-	if ((*RCC_BACKUPCONTROL & RCC_BACKUPCONTROL_LSEREADY) == 0) {
-		*RCC_BACKUPCONTROL |= RCC_BACKUPCONTROL_LSEENABLE;
-		while ((*RCC_BACKUPCONTROL & RCC_BACKUPCONTROL_LSEREADY) == 0) {}
+	if ((RCC->BDCR & rcc_BDCR_LSERDY) == 0) {
+		RCC->BDCR |= rcc_BDCR_LSEON;
+		while ((RCC->BDCR & rcc_BDCR_LSERDY) == 0) {}
 	}
 
-	u32 control = *RCC_BACKUPCONTROL;
-	control |= RCC_BACKUPCONTROL_RTCCLOCKENABLE;
-	control = (control & ~(RCC_BACKUPCONTROL_RTCCLOCKSELECT_MASK)) | RCC_BACKUPCONTROL_RTCCLOCKSELECT_LSE;
-	*RCC_BACKUPCONTROL = control;
+	u32 bdcr = RCC->BDCR;
+	bdcr |= rcc_BDCR_RTCEN;
+	bdcr = (bdcr & ~(rcc_BDCR_RTCSEL_MASK)) | rcc_BDCR_RTCSEL_LSE;
+	RCC->BDCR = bdcr;
 
 	RTC->CRH |= rtc_CRH_ALRIE;
 
